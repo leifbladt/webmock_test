2bff63a922f52f94132407bb69e77cae53d5a382
Leif Bladt <leif.bladt@gmx.de>
2013-06-28 09:32:23 +0200

    Extend test case with query values

--- test.rb
+++ test.rb
@@ -7,11 +7,11 @@ require 'net/http'
 require 'webmock'
 include WebMock::API
 
-stub_request(:get, 'http://www.example.com').to_return(status: 200, body: '')
+stub_request(:get, 'http://www.example.com?a=b').to_return(status: 200, body: '')
 
 uris = [
-  URI.parse('http://www.example.com/'),
-  Addressable::URI.parse('http://www.example.com/')
+  URI.parse('http://www.example.com/?a=b'),
+  Addressable::URI.parse('http://www.example.com/?a=b')
 ]
 
 uris.each do |uri|
